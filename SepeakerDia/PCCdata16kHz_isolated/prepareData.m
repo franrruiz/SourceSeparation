f05424ded47447d9a8b3a5f478bba28287b18600
ivaleraM <isabel.valera.martinez@gmail.com>
2015-03-16 19:01:28 +0100

    Speaker diarization data

---
+++ SepeakerDia/PCCdata16kHz_isolated/prepareData.m
@@ -0,0 +1,17 @@
+close all
+
+speakers=zeros(60*16000*2,2,5);
+
+for sp=1:5
+    t=0;
+    for sen=1:4
+        [data f]=wavread(['data/s' num2str(sp) '_' num2str(sen) '.wav']);
+        t=t+poissrnd((40.*rand)*16000);
+        speakers(t:t+size(data,1)-1,:,sp)=data;
+        t=t+size(data,1)-1;
+    end
+end
+
+ plot(squeeze(speakers(1:500:end,1,:)))
+ 
+ save('data.mat','speakers')
